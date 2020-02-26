06953244c896c6f996573f7d1cc9b1551baabc10
Alison Feldish <alison.feldish@gmail.com>
2020-02-25 20:20:06 -0500

    initial commit

---
+++ calculator.js
@@ -0,0 +1,28 @@
+var value1 = document.getElementById('value1')
+var value2 = document.getElementById('value2')
+var add = document.getElementById('button-add')
+var subtract = document.getElementById('button-subtract')
+var multiply = document.getElementById('button-multiply')
+var divide = document.getElementById('button-divide')
+var clear = document.getElementById('button-clear')
+var result = document.getElementById('result')
+
+add.addEventListener("click", function() {
+    result.innerHTML = parseInt(value1.value) + parseInt(value2.value)
+})
+
+subtract.addEventListener("click", function() {
+    result.innerHTML = parseInt(value1.value) - parseInt(value2.value)
+})
+
+multiply.addEventListener("click", function() {
+    result.innerHTML = parseInt(value1.value) * parseInt(value2.value)
+})
+
+divide.addEventListener("click", function() {
+    result.innerHTML = parseInt(value1.value) / parseInt(value2.value)
+})
+
+clear.addEventListener("click", function() {
+    result.innerHTML = ' ';
+})
